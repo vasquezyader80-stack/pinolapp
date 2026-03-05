94d0c709a7210b8283358d76cab3202a20616a81
vasquezyader80-stack <vasquezyader80@gmail.com>
2026-03-05 09:54:28 -0600

    Update Order.js

--- backend/backend/models/Order.js
+++ backend/backend/models/Order.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
 
+const OrderSchema = new mongoose.Schema({
+  userId: String,
+  businessId: String,
+  items: Array,
+  total: Number,
+  status: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
