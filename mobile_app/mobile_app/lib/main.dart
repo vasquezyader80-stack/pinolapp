d279bea112458aded3219da2622b03baf5dfcb23
vasquezyader80-stack <vasquezyader80@gmail.com>
2026-03-05 09:56:23 -0600

    Create main.dart

---
+++ mobile_app/mobile_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(PinolApp());
+}
+
+class PinolApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PinolApp',
+      theme: ThemeData(
+        primaryColor: Color(0xFF0067C6), // Azul oficial
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
