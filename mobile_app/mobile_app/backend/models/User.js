f72f6efe73628ec5dd33f943b6a01a8199489510
vasquezyader80-stack <vasquezyader80@gmail.com>
2026-03-05 09:53:01 -0600

    Update User.js

--- mobile_app/mobile_app/backend/models/User.js
+++ mobile_app/mobile_app/backend/models/User.js
@@ -1,1 +1,12 @@
 
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  phone: String,
+  role: String // 'cliente', 'repartidor', 'negocio'
+});
+
+module.exports = mongoose.model("User", UserSchema);
