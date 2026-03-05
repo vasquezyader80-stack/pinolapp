4986bfcace2b81df13703244b78ccbd814d2b632
vasquezyader80-stack <vasquezyader80@gmail.com>
2026-03-05 09:50:06 -0600

    Create server.js

---
+++ backend/backend/backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("PinolApp API funcionando 🇳🇮");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
+});
